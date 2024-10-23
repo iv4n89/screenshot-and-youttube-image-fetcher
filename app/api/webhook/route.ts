185a2f62fbea94e5cd771c180fca16b09292ff32
ivanbetanzos <e.aura.ape1.cdo@telefonica.com>
2024-10-23 17:55:02 +0200

    added webhook api

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,46 @@
+import { GITHUB_API_REPO } from "@/app/lib/constants";
+import fs from "fs";
+import { NextResponse } from "next/server";
+import path from "path";
+import puppeteer from "puppeteer";
+
+export async function POST() {
+  const response = await fetch(GITHUB_API_REPO);
+  const data = await response.json();
+  const filePath = path.join("data.json");
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const types = Object.keys(data);
+
+  for (const type of types) {
+    // Crear el directorio 'public/screenshots' si no existe
+    const screenshotsDir = path.join("public", "screenshots", type);
+    if (!fs.existsSync(screenshotsDir)) {
+      fs.mkdirSync(screenshotsDir, { recursive: true });
+    }
+
+    for (const item of data[type]) {
+      const { url, title } = item;
+      if (url.includes("youtu")) {
+        continue;
+      }
+      const fileName =
+        (title?.toLowerCase()?.replace(/\s+/g, "_") ??
+          url.replace(/https?:\/\//, "").replace(/\//g, "_")) + ".webp";
+      if (fs.existsSync(path.join(screenshotsDir, fileName))) {
+        console.log(`Skipping ${title}`);
+        continue;
+      }
+      await page.goto(url);
+      const filePath = path.join(screenshotsDir, fileName);
+      await page.screenshot({ path: filePath });
+    }
+  }
+
+  await browser.close();
+
+  return NextResponse.json({ message: "Data fetched and screenshots taken" });
+}
---
+++ app/lib/constants.ts
@@ -0,0 +1,3 @@
+
+export const GITHUB_API_REPO =
+"https://raw.githubusercontent.com/iv4n89/free_learning_resources/refs/heads/main/resources.json";
